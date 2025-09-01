f40e311f2bb75e00e49a4fcd7b52a55e3b72cde5
Ariane <arianenosferatu@gmail.com>
2025-09-01 20:01:26 -0300

    Importando e  ajustando estrutura do repositório

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryModule } from './cases/categories/category.module';
+import { BrandModule } from './cases/brands/brand.module';
+import { ProductModule } from './cases/products/product.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true, //só da para utilizar como True em desenvolvimento, pq tudo vai sincronizar com banco
+    }),
+    CategoryModule,
+    BrandModule,
+    ProductModule,
+  ],
+})
+export class AppModule {}
---
+++ src/cases/products/product.entity.ts
@@ -0,0 +1,27 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Brand } from '../brands/brand.entity';
+import { Category } from '../categories/category.entity';
+
+@Entity('Product')
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: false })
+  name: string;
+
+  @Column('text', { nullable: true })
+  description: string;
+
+  @Column('decimal', { nullable: false, precision: 10, scale: 2 })
+  price: number;
+
+  @Column('boolean', { nullable: false, default: true })
+  active: boolean;
+
+  @ManyToOne(() => Category, { eager: false, nullable: false })
+  category: Category; // igual ao pai
+
+  @ManyToOne(() => Brand, { eager: false, nullable: true })
+  brand: Brand;
+}
